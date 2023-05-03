f9bb1d15ce751cc489b4b2d54ed7535c94f08eb1
Nataliia <ruba.fish1500@gmail.com>
2023-05-03 09:08:35 +0300

    DAO tests added

--- src/test/java/com/studying/io/jdbc/application/PhoneModelDAOTest.java
+++ src/test/java/com/studying/io/jdbc/application/PhoneModelDAOTest.java
@@ -1,4 +1,104 @@
 package com.studying.io.jdbc.application;
 
+import com.studying.io.jdbc.application.model.PhoneModel;
+import com.sun.istack.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
 public class PhoneModelDAOTest {
+    @NotNull
+    private DAO<PhoneModel, String> dao;
+
+    @NotNull
+    private Connection connection;
+
+    @NotNull
+    private final PhoneModel model = new PhoneModel();
+
+    @Before
+    public void before() {
+        model.setName("test");
+        try {
+            String user = "postgres";
+            String password = "1";
+            String url = "jdbc:postgresql://localhost:5432/phones_magazine";
+            connection = DriverManager.getConnection(url, user, password);
+            dao = new PhoneModelDAO(connection);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @After
+    public void after() {
+        //Clean test PhoneModel.
+        dao.delete(model);
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Test
+    public void whenModelIsExistThenReturnPhoneModelWithId() {
+        final PhoneModel result = dao.read("samsung");
+        assertThat(result, is(new PhoneModel(1, "samsung")));
+    }
+
+
+    @Test
+    public void whenAddNewPhoneModelThenPhoneModelAddedAndReturnFalse() {
+        final boolean result = dao.create(model);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenAddPhoneModelWhichAlreadyExistThenReturnFalse() {
+        dao.create(model);
+        final boolean result = dao.create(model);
+        assertThat(result, is(false));
+    }
+
+
+    @Test
+    public void whenDeletePhoneModelWhichExistThenReturnTrue() {
+        dao.create(model);
+        final boolean result = dao.delete(model);
+        assertThat(result, is(true));
+    }
+
+
+    @Test
+    public void whenDeletePhoneModelWhichNotExistThenReturnFalse() {
+        final boolean result = dao.delete(model);
+        assertThat(result, is(false));
+    }
+
+
+    @Test
+    public void whenUpdateSuccessThenModelNameSetNewValue() {
+        dao.create(model);
+        model.setName("updated");
+        model.setId(dao.read("test").getId());
+        final boolean result = dao.update(model);
+        assertThat(result, is(true));
+        assertThat(dao.read("updated").getName(), is("updated"));
+    }
+
+    
+    @Test
+    public void whenTryUpdatePhoneModelWhichNotExistThenReturnFalse() {
+        final boolean result = dao.update(new PhoneModel(Integer.MAX_VALUE, "xxx"));
+        assertThat(result, is(false));
+    }
 }
--- src/test/java/com/studying/io/jdbc/application/UserDAOTest.java
+++ src/test/java/com/studying/io/jdbc/application/UserDAOTest.java
@@ -1,4 +1,117 @@
 package com.studying.io.jdbc.application;
 
+import com.studying.io.jdbc.application.model.User;
+import com.sun.istack.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+
 public class UserDAOTest {
+    @NotNull
+    private DAO<User, String> dao;
+
+    @NotNull
+    private Connection connection;
+
+    @Before
+    public void before() {
+        try {
+            String user = "postgres";
+            String password = "1";
+            String url = "jdbc:postgresql://localhost:5432/phones_magazine";
+            connection = DriverManager.getConnection(url, user, password);
+            dao = new UserDAO(connection);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @After
+    public void after() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Test
+    public void whenGetUserWhichExistThenReturnUser() {
+        final User user = dao.read("admin");
+        final User expected = new User();
+        expected.setLogin("admin");
+        expected.setPassword("123");
+        expected.setRole(new User.Role(1, "admin"));
+        assertThat(user, is(expected));
+    }
+
+
+    @Test
+    public void whenUserIsNotExistThenReturnEmptyUserObj() {
+        final User user = dao.read("xxx");
+        assertThat(user.getId(), is(-1));
+    }
+
+
+    @Test
+    public void whenAddUserWhichNotExistThenReturnUser() {
+        final User user = new User(0, "test", "test", new User.Role(1, "admin"));
+        final boolean result = dao.create(user);
+        assertThat(result, is(true));
+        //Clear test data.
+        dao.delete(dao.read("test"));
+    }
+
+
+    @Test
+    public void whenUserWhichExistDeletedThenReturnTrue() {
+        final User user = new User(0, "test", "test", new User.Role(1, "admin"));
+        dao.create(user);
+        final User state = dao.read("test");
+        boolean before = state.getId() != -1;
+        user.setId(state.getId());
+
+        final boolean after = dao.delete(user);
+        assertTrue(before);
+        assertTrue(after);
+    }
+
+
+    @Test
+    public void whenUserNotExistThenReturnFalse() {
+        assertFalse(dao.delete(new User(0, "test", "test", new User.Role(1, "admin"))));
+    }
+
+
+    @Test
+    public void whenUpdateExistUserThenPasswordUpdated() {
+        final User user = new User(0, "test", "test", new User.Role(1, "admin"));
+        dao.create(user);
+        final User gutted = dao.read("test");
+        gutted.setPassword("updated");
+        final boolean result = dao.update(gutted);
+        final User updated = dao.read("test");
+        assertThat(result, is(true));
+        assertThat(updated.getPassword(), is("updated"));
+
+        //Clear test data.
+        dao.delete(updated);
+    }
+
+
+    @Test
+    public void whenUpdateNotExistedUserThenPasswordUpdated() {
+        assertThat(dao.update(new User()), is(false));
+    }
 }
